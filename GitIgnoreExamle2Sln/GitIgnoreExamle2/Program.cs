cb985343b607bacc68f64e1909267f8bb3029abb
MTariqR <5100155@mypillar7uwc.ac.za>
2023-11-30 13:32:02 +0200

    initial commit

---
+++ GitIgnoreExamle2Sln/GitIgnoreExamle2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitIgnoreExamle2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
